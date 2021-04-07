be232f7562b9c91c70f00a88d2f2b3e3caffd332
yandif <52165593+yandif@users.noreply.github.com>
2021-04-07 15:57:44 +0800

    Update mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -3,9 +3,48 @@ import * as path from "https://deno.land/std@0.92.0/path/mod.ts";
 
 const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
 
-addEventListener("fetch", async (event: FetchEvent) => {
-  const url: string = path.resolve(__dirname, "./index.html");
-  const str: string = await Deno.readTextFile(url);
+addEventListener("fetch", (event: FetchEvent) => {
+  
+  const str: string =`<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Wendy</title>
+</head>
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  .app {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgb(218, 218, 28);
+  }
+
+  h1 {
+    font-size: 81px;
+    font-weight: bold;
+    color: white;
+  }
+
+</style>
+
+<body>
+  <div class="app">
+    <h1>Wendy</h1>
+  </div>
+</body>
+
+</html>`;
   const response = new Response(str, {
     headers: { "content-type": "text/html" },
   });
