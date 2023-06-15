63ec8890bcbe552f7ddb134663f5a98001247eaf
yandif <me.yandif@gmail.com>
2023-06-15 17:12:59 +0800

    feat(apps/react): 添加 平铺数据转树形数据 的工具函数

--- apps/react/src/utils/tool.ts
+++ apps/react/src/utils/tool.ts
@@ -19,3 +19,43 @@ export function unCompile(code: string) {
   }
   return c;
 }
+
+type TreeNode = {
+  id: number;
+  parent?: number;
+  children?: TreeNode[];
+};
+
+/**
+ * 把数据转为树形数据
+ */
+export function flatToTree<T extends TreeNode>(data: T[]) {
+  const map: Record<number, TreeNode & T> = {};
+  const treeData: (TreeNode & T)[] = [];
+
+  for (const item of data) {
+    const node = {
+      ...item,
+      children: [],
+    };
+    map[item.id] = node;
+  }
+
+  for (const item of data) {
+    const node = map[item.id];
+    if (item.parent === undefined) {
+      treeData.push(node);
+    } else {
+      const parent = map[item.parent];
+      parent.children?.push(node);
+    }
+  }
+
+  for (const value of Object.values(map)) {
+    if (value.children?.length === 0) {
+      delete value.children;
+    }
+  }
+
+  return treeData;
+}
