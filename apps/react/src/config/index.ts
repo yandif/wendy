9c160b35d3e0f1d6c37f37f9a75136309cc7aba0
yandif <me.yandif@gmail.com>
2023-06-17 18:57:55 +0800

    fix(apps/react): 删除多余常量

--- apps/react/src/config/index.ts
+++ apps/react/src/config/index.ts
@@ -1,16 +1,3 @@
 export const API_URL = import.meta.env.VITE_API_URL as string;
 export const NODE_ENV = import.meta.env.NODE_ENV as string;
 export const JWT_SECRET = import.meta.env.JWT_SECRET as string;
-
-export enum CodeEnum {
-  /** 成功 */
-  SUCCESS = 0,
-  /** 失败 */
-  FAILED = 1,
-  /** 用户名或密码错误 */
-  USERNAME_OR_PASSWORD_ERROR = 10040,
-  /** 没传递token */
-  NO_TOKEN = 10042,
-  /** token错误 */
-  TOKEN_ERROR = 10043,
-}
