6c5be415c1dce8bcc8133588661e72e61b8adfcb
yandif <me.yandif@gmail.com>
2023-07-21 11:10:33 +0800

    docs: 添加食谱demo

--- demos/CSS-in-JS-panda/panda.config.ts
+++ demos/CSS-in-JS-panda/panda.config.ts
@@ -1,4 +1,32 @@
 import { defineConfig } from '@pandacss/dev';
+import { defineRecipe } from '@pandacss/dev';
+
+export const buttonRecipe = defineRecipe({
+  name: 'button',
+  description: 'The styles for the Button component',
+  base: {
+    display: 'flex',
+  },
+  variants: {
+    visual: {
+      funky: { bg: 'red.200', color: 'white' },
+      edgy: { border: '1px solid {colors.red.500}' },
+    },
+    size: {
+      sm: { padding: '4', fontSize: '12px' },
+      lg: { padding: '8', fontSize: '40px' },
+    },
+    shape: {
+      square: { borderRadius: '0' },
+      circle: { borderRadius: 'full' },
+    },
+  },
+  defaultVariants: {
+    visual: 'funky',
+    size: 'sm',
+    shape: 'circle',
+  },
+});
 
 export default defineConfig({
   // Whether to use css reset
@@ -12,7 +40,11 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      recipes: {
+        button: buttonRecipe,
+      },
+    },
   },
   // emitPackage: true,
 
--- demos/CSS-in-JS-panda/src/App.tsx
+++ demos/CSS-in-JS-panda/src/App.tsx
@@ -4,8 +4,9 @@ import { Demo01 } from './demo/Demo01';
 import { Demo02 } from './demo/Demo02';
 import { Demo03 } from './demo/Demo03';
 import { Demo04 } from './demo/Demo04';
+import { Demo05 } from './demo/Demo05';
 
-const ComponentArr = [Demo01, Demo02, Demo03, Demo04];
+const ComponentArr = [Demo01, Demo02, Demo03, Demo04, Demo05];
 
 function App() {
   const [i, setI] = useState(0);
